747848f4396920649a436077db9a4d706d16cc42
Kabuñiang Buhawi Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-05-23 20:39:25 +0200

    huge change, characters of strings must be casted to unsigned char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -15,15 +15,16 @@
 //Compare 2 strings until n: return 0 if equal, else return s1 - s2
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n > 0)
+	size_t	i;
+
+	i = 0;
+	while (i < n)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		if (*s1 == '\0')
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if (s1[i] == '\0')
 			return (0);
-		s1++;
-		s2++;
-		n--;
+		i++;
 	}
 	return (0);
 }
