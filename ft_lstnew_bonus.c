45b53ea42143dc0877d24191bd5611544870ee3d
Kabuñiang Buhawi Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-05-23 20:45:04 +0200

    Create ft_lstnew_bonus.c

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmonjard <kmonjard@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 20:18:58 by kmonjard          #+#    #+#             */
+/*   Updated: 2025/05/23 20:19:01 by kmonjard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+//Allocates a node with content in it and have next point to NULL
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*out;
+
+	out = malloc(sizeof(t_list));
+	if (!out)
+		return (NULL);
+	out->content = content;
+	out->next = NULL;
+	return (out);
+}
